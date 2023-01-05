faf53c78ec92fbdef35bac21d3a7be402fd4bf00
KyryloSalo <salokirill2@gmail.com>
2023-01-05 22:49:52 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Річка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6150547980050813,
+          "pitch": 0.1128375910167918,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.7090973565428698,
+          "pitch": -0.171291311225513,
+          "title": "Знову я)",
+          "text": "Дивлюсь в айпад"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "Подвір‘я",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.7919810117731103,
+        "pitch": 0.36729620775238203,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09369610590758448,
+          "pitch": -0.054274833658015,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4202243643940147,
+          "pitch": -0.044834702186483,
+          "title": "Наш дім&nbsp;",
+          "text": "Я в (машинці)"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
